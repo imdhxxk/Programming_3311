3ccd2a71a96f6492cd0cd196124d752269494651
imdhxxk <barinlim77@gmail.com>
2026-03-20 10:09:53 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -5,13 +5,18 @@ let num = 0
                 
 // const resultH1 = document.getElementsByTagName("h1")[0]
 const resultH1 = document.getElementById("result")
-const plusButton = document.getElementsByClassName("plus")[0]
+// const plusButton = document.getElementsByClassName("plus")[0]
               
-// plusButton.addEventListener("click", () => {
+// // plusButton.addEventListener("click", () => {
+// //     num++
+// //     resultH1.innerHTML = num
+// // })
+// plusButton.onclick = () => {
 //     num++
 //     resultH1.innerHTML = num
-// })
-plusButton.onclick = () => {
+// }
+
+const plus = () => {
     num++
-    resultH1.innerHTML = num
+    resultH1.innerHTML = num;
 }

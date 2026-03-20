60872e6af57e5b22808e6e6349f56e193a8dc5e9
imdhxxk <barinlim77@gmail.com>
2026-03-20 10:04:28 +0900

    counter HTML, JS

---
+++ 02_counter/main.js
@@ -0,0 +1,17 @@
+// h1에 있는 숫자 요소를 가져오자
+// button에 있는 + 요소를 가져오자
+
+let num = 0
+                
+// const resultH1 = document.getElementsByTagName("h1")[0]
+const resultH1 = document.getElementById("result")
+const plusButton = document.getElementsByClassName("plus")[0]
+              
+// plusButton.addEventListener("click", () => {
+//     num++
+//     resultH1.innerHTML = num
+// })
+plusButton.onclick = () => {
+    num++
+    resultH1.innerHTML = num
+}

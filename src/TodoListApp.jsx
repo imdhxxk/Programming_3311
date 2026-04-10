97abc6547b742417188ad44ab7146748bab6c498
imdhxxk <barinlim77@gmail.com>
2026-04-10 09:01:33 +0900

    TodoListApp HTML classNaem, todolist.css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,25 +1,31 @@
+import './todolist.css';
+
 export default function TodoListApp() {
     return (
-        <div>
-            <h1>No Think, Do It.</h1>
-            <form>
-                <input type="text" name="todo" id="todo" /><button type="submit">Add</button>
+        <div className="todo">
+            <h1 className="todo__title">No Think, Do It.</h1>
+            <form className="todo__form">
+                <input type="text" name="todo" id="todo" className="todo__input" placeholder="할 일 입력해라"/>
+                <button type="submit" className="todo__button todo__button--add">Add</button>
             </form>
-            <ul id="TodoList">
-                <li>
-                    <input type="checkbox" name="check1" id="check1" /> 
-                    <label htmlFor="check1">Todo 1</label>
-                    <button>✏️</button>
-                    <button>❌</button>
+            <ul className="todo__list">
+                <li className="todo__item todo__item--empty">
+                    <p>할 일 없음.</p>
                 </li>
 
-                <li>
-                    <input type="checkbox" name="check2" id="check2" />
-                    <label htmlFor="check2">Todo 2</label>
-                    <button>✏️</button>
-                    <button>❌</button>
+                <li className="todo__item todo__item--complete">
+                    <input type="checkbox" name="check1" id="check1" className="todo__check"/>
+                    <label htmlFor="check1" className="todo__label">Todo 1</label>
+                    <button className="todo__button todo__button--edit">✏️</button>
+                    <button className="todo__button todo__button--delete">❌</button>
                 </li>
 
+                <li className="todo__item todo__item--complete">
+                    <input type="checkbox" name="check2" id="check2" className="todo__check"/>
+                    <label htmlFor="check2" className="todo__label">Todo 2</label>
+                    <button className="todo__button todo__button--edit">✏️</button>
+                    <button className="todo__button todo__button--delete">❌</button>
+                </li>
             </ul>
         </div>
     );
